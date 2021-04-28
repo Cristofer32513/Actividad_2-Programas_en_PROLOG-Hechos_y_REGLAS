2ad0a4a4782999f3a97a85bd196e95150fced1be
Cristofer32513 <cristofer32513@gmali.com>
2021-04-28 13:12:43 -0500

    Se agregaron las 3 bases de conocimientos.

---
+++ BC_Animales.pl
@@ -0,0 +1,26 @@
+% HECHOS
+%
+gato(macorina).
+gato(ushuaia).
+gato(lui).
+gato(felix).
+gato(ambar).
+gato(sparky).
+gato(luna).
+
+perro(solovino).
+perro(firulais).
+perro(macorina).
+
+color_gato(macorina,foca).
+color_gato(luna,negro).
+color_gato(lui,negro).
+color_gato(sparky,negro).
+color_gato(ambar,tricolor).
+color_gato(felix,tricolor).
+
+% REGLAS
+%
+%
+animal(A):-gato(A).       % VA animal(A) => gato(A)
+perro_gato(A):- gato(A),perro(A).      % es perro-gato => gato y perro
